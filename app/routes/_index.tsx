24bb00165341597aa62c711104353c38d3c4c2cb
Vojtech Adam <bluetrox@hotmail.com>
2023-04-08 19:11:42 +0200

    +++ Splitting calendar into horizontal clusters

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -38,11 +38,35 @@ const ROW_ENDS = {
   "13": 'row-end-13',
 }
 
+const ROWS = {
+  "1": 'grid-rows-1',
+  "2": 'grid-rows-2',
+  "3": 'grid-rows-3',
+  "4": 'grid-rows-4',
+  "5": 'grid-rows-5',
+  "6": 'grid-rows-6',
+  "7": 'grid-rows-7',
+  "8": 'grid-rows-8',
+  "9": 'grid-rows-9',
+  "10": 'grid-rows-10',
+  "11": 'grid-rows-11',
+  "12": 'grid-rows-12',
+  "13": 'grid-rows-13',
+}
+
 type Event = {
   id: number,
   startTime: keyof typeof ROW_STARTS,
   endTime: keyof typeof ROW_ENDS,
   color: string,
+}
+
+type Column = Array<Event>
+
+type Cluster = {
+  startTime: keyof typeof ROW_STARTS,
+  endTime: keyof typeof ROW_ENDS,
+  columns: Array<Column>,
 }
 
 // Here we have the source array populated with events - feel free to modify
@@ -92,57 +116,125 @@ const SOURCE_EVENTS: Array<Event>= [
   {
     id: 8,
     startTime: "5",
-    endTime: "12",
+    endTime: "10",
     color: "bg-blue-300",
   },
   {
     id: 9,
     startTime: "9",
-    endTime: "13",
+    endTime: "11",
     color: "bg-violet-300",
+  },
+  {
+    id: 10,
+    startTime: "9",
+    endTime: "11",
+    color: "bg-blue-300",
+  },
+  {
+    id: 11,
+    startTime: "4",
+    endTime: "5",
+    color: "bg-blue-300",
+  },
+  {
+    id: 12,
+    startTime: "12",
+    endTime: "13",
+    color: "bg-violet-400",
+  },
+  {
+    id: 13,
+    startTime: "12",
+    endTime: "13",
+    color: "bg-blue-300",
   },
 ]
 
 
-const findSuitableColumn = (event: Event, columns: Array<Array<Event>>) => {
+const findSuitableColumn = (event: Event, columns: Array<Column>):[number, boolean] => {
+  let columnIndex = null
+  let newCluster = true
   // Iterate through columns
   for (const [index, column] of columns.entries()) {
     // Look for a colliding event
     const collision = Boolean(column.find(existingEvent => 
       parseInt(event.startTime) < parseInt(existingEvent.endTime)
     ))
-    // If there is none, the current event can be placed into this column
-    if (!collision) {
-      return index
+    // If there is no collission and no column is set yet,
+    // the current event can be placed into this column
+    if (!collision && !columnIndex) {
+      columnIndex = index
+    }
+    // If there is a collision, the event should be part of the current cluster
+    if (collision) {
+      newCluster = false
     }
   }
-  // Else return index of a new column
-  return columns.length
+  // If no suitable column is found, add one
+  if (columnIndex === null) {
+    columnIndex = columns.length
+  }
+  return [columnIndex, newCluster]
 }
 
-const divideEventsIntoColumns = (eventsArray: Array<Event>) => {
-  let columns: Array<Array<Event>> = []
+const divideEventsIntoClusters = (eventsArray: Array<Event>) => {
+  let clusters: Array<Cluster> = [{
+    startTime: "1",
+    endTime: "2",
+    columns: []
+  }]
 
   // Sort events based on startTime
   eventsArray.sort((a: Event, b: Event) => parseInt(a.startTime) - parseInt(b.startTime))
   
-  // Divide events into columns
+  // Divide events into clusters
   eventsArray.forEach(event => {
-    const columnIndex = findSuitableColumn(event, columns)
-    // If the column exists, add the event
-    if(columns[columnIndex] !== undefined) {
-      columns[columnIndex].push(event)
-    // Else add the event to a new column
+    // Select last cluster in array
+    const lastCluster = clusters[clusters.length-1]
+    // Determine column and existing/new cluster
+    const [columnIndex, newCluster] = findSuitableColumn(event, lastCluster.columns)
+    // Create a new cluster
+    if (newCluster && lastCluster.columns.length > 0) {
+      // When creating a new cluster, set the end of the previous one
+      // where the new one begins
+      lastCluster.endTime = event.startTime
+      clusters.push({
+        startTime: event.startTime,
+        endTime: event.endTime,
+        columns: [[event]],
+      })
+    // Else add the event to an existing cluster
     } else {
-      columns.push([event])
+      // If the column exists, add the event
+      if(lastCluster.columns[columnIndex] !== undefined) {
+        lastCluster.columns[columnIndex].push(event)
+      // Else add the event to a new column
+      } else {
+        lastCluster.columns.push([event])
+      }
+      // Adjust cluster startTime and endTime
+      if (
+        lastCluster.columns.length &&
+        parseInt(lastCluster.startTime) > parseInt(event.startTime)
+      ) {
+        lastCluster.startTime = event.startTime
+      }
+      if (
+        lastCluster.columns.length &&
+        parseInt(lastCluster.endTime)< parseInt(event.endTime)
+      ) {
+        lastCluster.endTime = event.endTime
+      }
     }
+    
   })
-  return columns
+  return clusters
 }
 
 export default function Index() {
   
-  const columnsWithEvents = divideEventsIntoColumns(SOURCE_EVENTS)
+  const clustersWithEvents = divideEventsIntoClusters(SOURCE_EVENTS)
   
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
@@ -166,36 +258,56 @@ export default function Index() {
             ))}
           </div>
         </div>
-        {columnsWithEvents.map((column, index) => (
-          <div
-            key={index}
-            className="flex-1 px-1"
-          >
-            <div
-              className="
-                grid grid-rows-12 grid-flow-col gap-2 h-full
-                font-mono text-white text-sm text-center font-bold
-                leading-6 bg-stripes-fuchsia rounded-lg
-              "
-            >
-              {column.map(event => {
-                return(
+        <div className="flex-1">
+          {clustersWithEvents.map((cluster, clusterIndex) => {
+            // Determine cluster length and assign appropriate number of rows
+            const clusterLength: number = parseInt(cluster.endTime) - parseInt(cluster.startTime)
+            const rowsClass = clusterLength.toString() in ROWS ? ROWS[clusterLength.toString() as keyof typeof ROWS] : ROWS["1"]
+
+            return (
+              <div 
+                key={clusterIndex}
+                className="flex pb-2"
+              >
+                {cluster.columns.map((column, index) => (
                   <div
-                    key={event.id}
-                    className={
-                      `p-4 rounded-lg shadow-lg bg-blue-500
-                      ${ROW_STARTS[event.startTime]} ${ROW_ENDS[event.endTime]}
-                      ${event.color}`
-                    }
+                    key={index}
+                    className="flex-1 px-1"
                   >
-                    <p>ID: {event.id}</p>
-                    <p>From: {event.startTime} To: {event.endTime}</p>
+                    <div
+                      className={`
+                        grid ${rowsClass} grid-flow-col gap-2 h-full
+                        font-mono text-white text-xs md:text-sm text-center font-bold
+                        leading-6 bg-stripes-fuchsia rounded-lg
+                      `}
+                    >
+                      {column.map(event => {
+                        // Calculate event grid position relative to its cluster
+                        const offsetStartTime: number = parseInt(event.startTime)-parseInt(cluster.startTime)+1
+                        const offsetEndTime: number = parseInt(event.endTime)-parseInt(cluster.startTime)+1
+
+                        return(
+                          <div
+                            key={event.id}
+                            className={
+                              `p-2 md:p-4 rounded-lg shadow-lg bg-blue-500
+                              ${ROW_STARTS[offsetStartTime.toString() as keyof typeof ROW_STARTS]}
+                              ${ROW_ENDS[offsetEndTime.toString() as keyof typeof ROW_ENDS]}
+                              ${event.color}`
+                            }
+                          >
+                            <p>ID: {event.id}</p>
+                            <p>From: {event.startTime} To: {event.endTime}</p>
+                          </div>
+                        )
+                      })}
+                    </div>
                   </div>
-                )
-              })}
-            </div>
-          </div>
-        ))}
+                ))}
+              </div>
+            )
+          })}
+        </div>
       </div>      
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
   theme: {
     extend: {
       gridTemplateRows: {
-        '12': 'repeat(12, minmax(0, 1fr))',
+        '1': 'repeat(1, minmax(100px, 100px))',
+        '2': 'repeat(2, minmax(100px, 100px))',
+        '3': 'repeat(3, minmax(100px, 100px))',
+        '4': 'repeat(4, minmax(100px, 100px))',
+        '5': 'repeat(5, minmax(100px, 100px))',
+        '6': 'repeat(6, minmax(100px, 100px))',
+        '7': 'repeat(7, minmax(100px, 100px))',
+        '8': 'repeat(8, minmax(100px, 100px))',
+        '9': 'repeat(9, minmax(100px, 100px))',
+        '10': 'repeat(10, minmax(100px, 100px))',
+        '11': 'repeat(11, minmax(100px, 100px))',
+        '12': 'repeat(12, minmax(100px, 100px))',
+        '13': 'repeat(13, minmax(100px, 100px))',
       },
       gridRowStart: {
         '8': '8',
